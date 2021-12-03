98004b6c0433f6181b030e428cc131d0720d20c5
zawadi ngowa <rachaelngowa@yahoo.com>
2021-12-03 17:21:45 +0000

    multiplication

--- multiplication.js
+++ multiplication.js
@@ -1,8 +1,8 @@
-const num1 = 5
-const num2 = 3
+const num1 = 5;
+const num2 = 3;
 
 //multiply two numbers
-const multiply = num1 * num2
+const multiply = num1 * num2;
 
 //display multiplication
-console.log('the multiplication of ' * num1 * 'and' * num2 * ' is:' * multiply)
+console.log('the multiplication of ' * num1 * 'and' * num2 * ' is:' * multiply);
