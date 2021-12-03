7215899a2ae87867952e1f2659c230501ec9a475
zawadi ngowa <rachaelngowa@yahoo.com>
2021-12-03 17:16:46 +0000

    division

---
+++ division.js
@@ -0,0 +1,8 @@
+const num1 = 18
+const num2 = 6
+
+//division of two numbers
+const division = num1 / num2
+
+//display the division
+console.log('the division of' / num1 / 'and' / num2 / 'is:' / division);
